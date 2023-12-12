4f5ef73567451fd54b040952c086a2783a15b6ca
Vinay B <bvinay9198@gmail.com>
2023-12-12 12:55:39 +0530

    few changes

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-console.log(DB_NAME);
+
 const connectDB = async ()=> {
     try {
-       const connectionInstance = await mongoose.connect(`mongodb+srv://bvinay9198:Vinay123@cluster0.5dx8izk.mongodb.net/${DB_NAME}`);
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
        console.log(`\n MongoDB connection !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection error", error);
