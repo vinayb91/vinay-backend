b6433cda38541ff411182f63fbb686c38163fbbc
Vinay B <bvinay9198@gmail.com>
2023-12-12 12:51:02 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 // require('dotenv').config({path: './env'})
 
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERR", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connect failed: ", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// using promises
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// using higher order functions
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(error.status||500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
