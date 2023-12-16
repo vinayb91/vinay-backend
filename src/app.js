8550884589b83de1342d08b321d0ec81a7cfa3ca
Vinay B <bvinay9198@gmail.com>
2023-12-16 18:58:40 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ app.use(express.urlencoded({limit: "16kb", extended: true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from './routes/user.routes.js'
 
 
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/v1/users/register
 export { app }
